0d7459c0bbb58df8248564f9a89178a71a83d64e
Ozcan CAVGA <cavga1997@gmail.com>
2022-11-11 10:33:02 +0300

    odev4 yapildi

---
+++ app_server/models/db.js
@@ -0,0 +1,23 @@
+var mongoose=require("mongoose");
+require("./mekansema")
+ //var dbURI = "mongodb://localhost/mekanbul";
+var dbURI = "mongodb+srv://ozcan:ozcan1234@mekanbul.bnvzmrx.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(dbURI);
+function kapat(msg,callback){
+    mongoose.connection.close(function(){
+        console.log(msg);
+        callback();
+    }
+    );
+}
+process.on("SIGINT",function(){
+    kapat("Uygulama Kapatıldı",function(){
+        process.exit(0);
+    }
+    );
+} 
+);//sigint sinyali geldiğinde kapat fonksiyonu ile eşleştirir çağrılır
+mongoose.connection.on("connected",function(){
+    console.log(dbURI+" adresindeki veritabanına bağlandı")
+}
+);
