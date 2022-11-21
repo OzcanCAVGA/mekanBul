e7a512d90a2f6d45f928f814f8af95cccc32d519
Ozcan CAVGA <cavga1997@gmail.com>
2022-11-21 12:36:31 +0300

    odev5

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
+
 var createError = require('http-errors');
-require('./app_server/models/db'); 
+//require('./app_server/models/db'); 
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var cookieParser=require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./app_server/routes/index');
+require('./app_api/models/db');
+var indexRouter=require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
-
+var apiRouter=require('./app_api/routes/index');
 var app = express();
-
+app.use("/api",apiRouter);
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
